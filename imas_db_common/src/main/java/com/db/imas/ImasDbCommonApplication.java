4ba9e4bc3f13598076e2f14d4a5d050c46d17b99
noname <sakuraoka01@gmail.com>
2021-06-10 17:49:20 +0800

    init common

---
+++ imas_db_common/src/main/java/com/db/imas/ImasDbCommonApplication.java
@@ -0,0 +1,13 @@
+package com.db.imas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImasDbCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImasDbCommonApplication.class, args);
+    }
+
+}
